380ce652c1663279193a9582a378361f271cf05c
Benjamin <bthomas@caronbletzer.com>
2025-07-15 13:06:10 -0400

    Static file extraction final

---
+++ server/utils/blobUploader.js
@@ -0,0 +1,51 @@
+const {
+  BlobServiceClient,
+  StorageSharedKeyCredential,
+  generateBlobSASQueryParameters,
+  BlobSASPermissions,
+  SASProtocol,
+} = require("@azure/storage-blob");
+
+require("dotenv").config({ path: "../.env" });
+
+const accountName = process.env.AZURE_STORAGE_ACCOUNT;
+const accountKey = process.env.AZURE_STORAGE_KEY;
+const containerName = process.env.AZURE_CONTAINER_NAME;
+
+const sharedKeyCredential = new StorageSharedKeyCredential(
+  accountName,
+  accountKey
+);
+
+const blobServiceClient = new BlobServiceClient(
+  `https://${accountName}.blob.core.windows.net`,
+  sharedKeyCredential
+);
+
+async function uploadAndGenerateSAS(fileBuffer, originalName) {
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+
+  const blobName = `uploads/${Date.now()}-${originalName}`;
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  await blockBlobClient.uploadData(fileBuffer, {
+    blobHTTPHeaders: { blobContentType: "application/pdf" },
+  });
+
+  const expiresOn = new Date(Date.now() + 5 * 60 * 1000); // 5 min SAS token
+  const sasToken = generateBlobSASQueryParameters(
+    {
+      containerName,
+      blobName,
+      permissions: BlobSASPermissions.parse("r"),
+      expiresOn,
+      protocol: SASProtocol.Https,
+    },
+    sharedKeyCredential
+  ).toString();
+
+  const blobUrl = `${blockBlobClient.url}?${sasToken}`;
+  return blobUrl;
+}
+
+module.exports = uploadAndGenerateSAS;
